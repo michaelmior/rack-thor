d75a8e84d0833240b381c52f07b01243cff3dfe2
Michael Mior <mmior@uwaterloo.ca>
2018-02-24 11:37:30 -0500

    Switch to POST and enable options and args

--- lib/rack-thor.rb
+++ lib/rack-thor.rb
@@ -8,14 +8,15 @@ class RackThor
     @cli = cli
     @router = Hanami::Router.new
     cli.all_commands.each do |name, c|
-      @router.get "/#{name}", to: ->(env) do
+      @router.post "/#{name}", to: ->(env) do
+        req = Rack::Request.new(env)
         begin
           $stdout = StringIO.new
           command = @cli.all_commands[name]
-          @cli.new([], {}, {
+          @cli.new([], req.params['options'] || {}, {
             current_command: command,
             command_options: command.options
-          }).invoke(name)
+          }).invoke(name, req.params['args'] || [])
           $stdout.close
           [200, {}, [$stdout.string]]
         ensure
--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -1,9 +1,16 @@
 require 'thor'
 
 class TestCLI < Thor
-  desc 'test', 'Command'
-  def test
+  desc 'foo', 'Command'
+  def foo
     puts '{}'
+  end
+
+  desc 'bar', 'Command'
+  method_options baz: :string
+  def bar(corge)
+    puts options[:baz]
+    puts corge
   end
 end
 
@@ -12,9 +19,15 @@ describe 'test app' do
     RackThor.new(TestCLI).router
   end
 
-  it 'can run a command' do
-    get '/test'
+  it 'can run a simple command' do
+    post '/foo'
     expect(last_response).to be_ok
     expect(last_response.body).to eq("{}\n")
+  end
+
+  it 'can run a command with options' do
+    post '/bar', {args: ['grault'], options: {baz: 'quux'}}
+    expect(last_response).to be_ok
+    expect(last_response.body).to eq("quux\ngrault\n")
   end
 end
